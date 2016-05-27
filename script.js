83d3959252e3327494ac43a09dafc49ee04c2983
343_Barinova <nimfadorka@mail.ru>
2016-05-27 14:07:54 +0300

    DONE

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+$('#1').addClass('font');
+$('#2').addClass('header');
+$('#3').addClass('tr');
+$('#4').addClass('text');
+
+$('#menu').addClass('menu');
+$('#menus').addClass('menus');
+$('#menus').children().addClass('menus__item');
